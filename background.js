c23c90d0fe38dabb4764590fe89a757d2f14d695
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-09-23 09:50:30 +0500

    Initial commit with basic functionality

---
+++ background.js
@@ -0,0 +1,93 @@
+'use strict';
+
+chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
+    var handler = {
+
+        getTabs: function () {
+            chrome.tabs.query({}, function (tabs) {
+                sendResponse(tabs);
+            });
+
+            return true;
+        },
+
+        switchTab: function (params) {
+            chrome.tabs.update(params.tabId, {active: true}, function () {
+                chrome.windows.update(params.windowId, {focused: true});
+            });
+
+            return true;
+        },
+
+        closeTab: function (params) {
+
+            chrome.tabs.remove(params.tabId);
+
+            return true;
+        }
+    };
+
+    return handler[req.type](req.params);
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+
+    if (changeInfo.status !== 'loading')
+        return;
+
+    chrome.tabs.executeScript(tabId, {
+        code: 'var injected = window.tabSwitcherInjected; window.tabSwitcherInjected = true; injected;',
+        runAt: 'document_start'
+    }, function (res) {
+
+        if (chrome.runtime.lastError ||  // don't continue if error
+            res[0]) // value of `injected` above: don't inject twice
+            return;
+
+        var cssFiles = [
+                'tab-switcher.css'
+            ],
+            jsFiles = [
+                'jquery.js',
+                'keymaster.js',
+                'fuse.js',
+                'tab-switcher.js'
+            ];
+
+        eachTask([function (cb) {
+            return eachItem(cssFiles, inject('insertCSS'), cb);
+        }, function (cb) {
+            return eachItem(jsFiles, inject('executeScript'), cb);
+        }]);
+
+        function inject(fn) {
+            return function (file, cb) {
+                chrome.tabs[fn](tabId, {file: file, runAt: 'document_start'}, cb);
+            };
+        }
+    });
+
+});
+
+function eachTask(tasks, done) {
+    (function next() {
+        var index = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+
+        if (index === tasks.length) {
+            done && done();
+        } else {
+            tasks[index](function () {
+                return next(++index);
+            });
+        }
+    })();
+}
+
+function eachItem(arr, iter, done) {
+    var tasks = arr.map(function (item) {
+        return function (cb) {
+            return iter(item, cb);
+        };
+    });
+    return eachTask(tasks, done);
+}
---
+++ tab-switcher.js
@@ -0,0 +1,156 @@
+'use strict';
+
+$(document).ready(function () {
+
+    var MAIN_TEMPLATE = '<div class="tab-switcher" style="display: none;">' +
+                            '<input type="text">' +
+                            '<ul class="tabs-list">' +
+                            '</ul>' +
+                        '</div>',
+        TAB_SWITCHER  = '.tab-switcher',
+        DOWN_KEY      = 40,
+        UP_KEY        = 38,
+        ESCAPE_KEY    = 27,
+        ENTER_KEY     = 13,
+        SEMICOLON_KEY = 186,
+        HOLDER_IMG    = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAAMklEQVR4AWMgEkT9R4INWBUgKX0Q1YBXQYQCkhKEMDILogSnAhhEV4AGRqoCTEhkPAMAbO9DU+cdCDkAAAAASUVORK5CYII=',
+        MASTER_KEY    = '⌘+⇧+k, ⌃+⇧+k',
+        TAB_TEMPLATE  = '<li data-tab-id="{id}" data-window-id="{windowId}" class="tab-item">' +
+                            '<img src="{favicon}" class="left" onerror="this.src=\'' + HOLDER_IMG + '\'">' +
+                            '<span>{title}</span>' +
+                        '</li>';
+
+    function loadExtension() {
+        var $html = $('html');
+        var $document = $(document);
+
+        $html.append(MAIN_TEMPLATE);
+
+        bindUI();
+    }
+
+    function bindUI() {
+        var $tabSwitcher = $(TAB_SWITCHER);
+
+        $(document).on('blur', TAB_SWITCHER, function () {
+            $tabSwitcher.hide();
+            $tabSwitcher.find('input').val('');
+        });
+
+        $(document).on('keydown', function (e) {
+
+            var keyCode = e.keyCode,
+                goingUp        = (keyCode === UP_KEY),
+                goingDown      = (keyCode === DOWN_KEY),
+                escaping       = (keyCode === ESCAPE_KEY),
+                switchingTab   = (keyCode === ENTER_KEY),
+                closingTab     = (keyCode === SEMICOLON_KEY),
+                $firstSelected = $tabSwitcher.find('.tab-item.selected').first();
+
+            // Switcher was visible and either down or up key was pressed
+            if ($tabSwitcher.is(':visible')) {
+
+                if ((goingUp || goingDown)) {
+                    var $firstTab      = $tabSwitcher.find('.tab-item').first(),
+                        $lastTab       = $tabSwitcher.find('.tab-item').last(),
+                        $nextSelected;
+
+                    if ($firstSelected.length === 0) {
+                        $nextSelected = $firstTab.addClass('selected');
+                    } else {
+                        $firstSelected.removeClass('selected');
+
+                        var $nextTab = $firstSelected.next('.tab-item'),
+                            $prevTab = $firstSelected.prev('.tab-item');
+
+                        if (goingDown) {
+
+                            if ($nextTab.length !== 0) {
+                                $nextSelected = $nextTab.addClass('selected');
+                            } else {
+                                $nextSelected = $firstTab.addClass('selected');
+                            }
+
+                        } else if (goingUp) {
+                            if ($prevTab.length !== 0) {
+                                $nextSelected = $prevTab.addClass('selected');
+                            } else {
+                                $nextSelected = $lastTab.addClass('selected');
+                            }
+                        }
+                    }
+
+                    $nextSelected.get(0).scrollIntoViewIfNeeded();
+                } else if (escaping) {
+                    $tabSwitcher.hide();
+                } else if (switchingTab && $firstSelected.length !== 0) {
+                    switchToTab($firstSelected.data('tabId'), $firstSelected.data('windowId'));
+                } else if (closingTab && $firstSelected.length !== 0) {
+                    e.preventDefault();
+
+                    if (closeTab($firstSelected.data('tabId'))) {
+                        $firstSelected.remove();
+                    }
+                }
+            }
+        });
+
+        key(MASTER_KEY, function () {
+            $(TAB_SWITCHER).show().find('input').focus();
+            populateTabs();
+        });
+    }
+
+    function getTabsHtml(tabs) {
+        var tabsHtml = '';
+        tabs.forEach(function(tab){
+
+            var tempTabTemplate = TAB_TEMPLATE,
+                faviconUrl = tab.favIconUrl || HOLDER_IMG;
+
+            tempTabTemplate = tempTabTemplate.replace('{favicon}', faviconUrl);
+            tempTabTemplate = tempTabTemplate.replace('{title}', tab.title);
+            tempTabTemplate = tempTabTemplate.replace('{id}', tab.id);
+            tempTabTemplate = tempTabTemplate.replace('{windowId}', tab.windowId);
+
+            tabsHtml += tempTabTemplate;
+        });
+
+        return tabsHtml;
+    }
+
+    function populateTabs() {
+        chrome.extension.sendMessage({ type: 'getTabs' }, function(tabs) {
+            if (!tabs) {
+                return false;
+            }
+
+            var tabsHtml = getTabsHtml(tabs);
+            $('.tabs-list').html(tabsHtml);
+        });
+    }
+
+    function switchToTab(tabId, windowId) {
+        chrome.extension.sendMessage({
+            type: 'switchTab',
+            params: {
+                tabId: tabId,
+                windowId: windowId
+            }
+        }, function(res) {});
+    }
+
+    function closeTab(tabId) {
+
+        chrome.extension.sendMessage({
+            type: 'closeTab',
+            params: {
+                tabId: tabId
+            }
+        }, function(res) {});
+
+        return true;
+    }
+
+    loadExtension();
+});
